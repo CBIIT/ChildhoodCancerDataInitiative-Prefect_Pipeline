5aa82a440c9b05c1b2b00b0f4be6a1aab19ea796
qiongL910 <33666806+qiongL910@users.noreply.github.com>
2024-01-09 15:30:16 -0600

    Update s3_catcherry.py

--- src/s3_catcherry.py
+++ src/s3_catcherry.py
@@ -583,22 +583,6 @@ def CatchERRy(file_path: str, template_path: str):  # removed profile
     #
     ##############
 
-    catcherr_logger.info("Writing out the CatchERR")
-
-#    template_workbook = openpyxl.load_workbook(template_path)
-
-
-#    for sheet_name, df in meta_dfs.items():
-#        # select workbook tab
-#        ws = template_workbook[sheet_name]
-#        # V Not needed as the template is set or atleast more consistently clean.
-#        # remove any data that might be in the template
-#        ws.delete_rows(2, ws.max_row)
-#
-#        # write the data
-#        for row in dataframe_to_rows(df, index=False, header=False):
-#            ws.append(row)
-
     catcherr_logger.info("Writing out the CatchERR using pd.ExcelWriter")
     # save out template
     catcherr_out_file = f"{output_file}.xlsx"
