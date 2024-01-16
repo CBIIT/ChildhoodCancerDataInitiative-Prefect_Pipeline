6e0f0fdbc39b0ea303d346b5a1ac1612e9f7a4a9
qiongL910 <liuqiong891023@gmail.com>
2024-01-16 13:00:51 -0600

    CCDIDC-749 added unittests for bucket reader

--- tests/test_read_buckets.py
+++ tests/test_read_buckets.py
@@ -3,10 +3,17 @@ import sys
 import datetime
 import pytest
 import pandas as pd
+import mock
+from prefect.testing.utilities import prefect_test_harness
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir)
-from src.read_buckets import extract_obj_info, count_df, single_bucket_content_str
+from src.read_buckets import (
+    extract_obj_info,
+    count_df,
+    single_bucket_content_str,
+    read_bucket_content,
+)
 
 
 @pytest.fixture
@@ -27,6 +34,44 @@ def single_object_noext_return():
         "LastModified": datetime.date(2024, 3, 4),
     }
     return object_dict
+
+@pytest.fixture
+def paginate_return_iter():
+    iterator_list = iter(
+        [
+            {
+                "IsTruncated": True,
+                "Contents": [
+                    {
+                        "Key": "my/mock/dir/file.txt",
+                        "Size": 12345,
+                        "LastModified": datetime.date(2023, 11, 12),
+                    },
+                    {
+                        "Key": "my/mock/dir/file.txt",
+                        "Size": 23456,
+                        "LastModified": datetime.date(2023, 11, 13),
+                    },
+                ],
+            },
+            {
+                "IsTruncated": False,
+                "Contents": [
+                    {
+                        "Key": "my/mock/dir/file_no_extension",
+                        "Size": 34567,
+                        "LastModified": datetime.date(2023, 11, 13),
+                    },
+                    {
+                        "Key": "my/mock/dir/file.fastq",
+                        "Size": 45678,
+                        "LastModified": datetime.date(2023, 11, 14),
+                    },
+                ],
+            },
+        ]
+    )
+    return iterator_list
 
 
 def test_extract_obj_info_w_extension(single_object_return):
@@ -68,8 +113,24 @@ def test_single_bucket_content_str():
         ext_dict=pd.DataFrame(ext_dict),
         date_dict=pd.DataFrame(date_dict),
     )
-    print(test_str)
     assert "34.5 MB" in test_str
     assert "| .txt        |       2 |\n" in test_str
     assert "| 2023-12-23 |       5 |\n" in test_str
     assert "789\n" in test_str
+
+
+@mock.patch("src.read_buckets.set_s3_session_client", autospec=True)
+def test_read_bucket_content(mock_s3_client, paginate_return_iter):
+    with prefect_test_harness():
+        # run the flow on a mocked s3 client return
+        s3_client = mock_s3_client.return_value
+        mock_paginator = s3_client.get_paginator.return_value
+        mock_paginator.paginate.return_value =  paginate_return_iter
+
+        bucket_size, file_count, file_ext_df, modified_date_df = read_bucket_content(
+            bucket="mock_bucket"
+        )
+        assert bucket_size == 12345 + 23456 + 34567 + 45678
+        assert file_count == 4
+        assert file_ext_df.shape[0] == 3
+        assert modified_date_df.shape[0] == 3
