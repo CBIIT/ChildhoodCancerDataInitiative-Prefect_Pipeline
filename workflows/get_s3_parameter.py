857caebf619a3d70025b40e4275c4206e368ebd1
qiongL910 <liuqiong891023@gmail.com>
2024-02-21 12:56:52 -0600

    CCDIDC-861 added get-parameter workflow

---
+++ workflows/get_s3_parameter.py
@@ -0,0 +1,30 @@
+from prefect import flow, get_run_logger
+import os
+import sys
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir)
+from src.utils import set_s3_session_client
+from botocore.exceptions import ClientError
+import json
+
+@flow
+def get_s3_parameter(parameter_name: str):
+    # get logger
+    logger = get_run_logger()
+
+    # get s3_client object
+    s3_client = set_s3_session_client()
+
+    try:
+        parameter_response =  s3_client.get_parameter(Name=parameter_name)
+        logger.info(f"Parameter info:\n{json.dump(parameter_response, indent=4)}")
+    except ClientError as err:
+        ex_code = err.response["Error"]["Code"]
+        ex_message = err.response["Error"]["Message"]
+        logger.error(ex_code + ":" + ex_message)
+    except Exception as error:
+        logger.error(f"Get s3 parameter {parameter_name} FAILED")
+        logger.error("General exception noted.", exc_info=True)
+
+    return None
