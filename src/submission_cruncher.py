6d6095414360955d27db665489cefa05d05031c5
qiongL910 <liuqiong891023@gmail.com>
2024-04-01 12:41:05 -0500

    changed dropna parameter

--- src/submission_cruncher.py
+++ src/submission_cruncher.py
@@ -16,6 +16,7 @@ def if_version_match(xlsx_list: list[str], template_version: str) -> tuple[list]
             if_not_match.append((i, i_version))
     return if_match, if_not_match
 
+
 @task(log_prints=True)
 def append_one_submission(submission_file: str, append_to_file: str):
     """
@@ -41,7 +42,7 @@ def append_one_submission(submission_file: str, append_to_file: str):
     warnings.simplefilter(action="ignore", category=UserWarning)
     submission_obj = pd.ExcelFile(submission_file)
     append_to_obj = pd.ExcelFile(append_to_file)
-    skip_sheetnames =  ["README and INSTRUCTIONS","Dictionary","Terms and Value Sets"]
+    skip_sheetnames = ["README and INSTRUCTIONS", "Dictionary", "Terms and Value Sets"]
     na_bank = ["NA", "na", "N/A", "n/a", ""]
     sheetnames = submission_obj.sheet_names
     sheetnames = [i for i in sheetnames if i not in skip_sheetnames]
@@ -50,7 +51,7 @@ def append_one_submission(submission_file: str, append_to_file: str):
             submission_obj, sheet_name=j, na_values=na_bank, dtype="string"
         )
         # test if the df is empty
-        j_df.drop(columns=["type"], inplace=True).dropna(how="all", inplace=True)
+        j_df = j_df.drop(columns=["type"]).dropna(how="all")
         if j_df.empty:
             pass
         else:
@@ -60,9 +61,7 @@ def append_one_submission(submission_file: str, append_to_file: str):
                 na_values=na_bank,
                 dtype="string",
             )
-            j_append_to_df.drop(columns=["type"], inplace=True).dropna(
-                how="all", inplace=True
-            )
+            j_append_to_df = j_append_to_df.drop(columns=["type"]).dropna(how="all")
             j_append_to_df = pd.concat([j_append_to_df, j_df], ignore_index=True)
             j_append_to_df.drop_duplicates(inplace=True, ignore_index=True)
             j_append_to_df["type"] = j
@@ -84,9 +83,13 @@ def concatenate_submissions(xlsx_list: list[str], template_file: str, logger) ->
     # check if submisison files' version matches to template's
     tempalte_version = CheckCCDI(ccdi_manifest=template_file).get_version()
     logger.info(f"CCDI template version: {tempalte_version}")
-    xlsx_list, not_matched_list = if_version_match(xlsx_list=xlsx_list, template_version=tempalte_version)
+    xlsx_list, not_matched_list = if_version_match(
+        xlsx_list=xlsx_list, template_version=tempalte_version
+    )
     if len(not_matched_list) != 0:
-        logger.error(f"Found {len(not_matched_list)} submission files has version different from template:  {*not_matched_list,}")
+        logger.error(
+            f"Found {len(not_matched_list)} submission files has version different from template:  {*not_matched_list,}"
+        )
     else:
         logger.info("Submission files version matches to template's")
 
