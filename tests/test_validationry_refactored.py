1595712da79cbc67e60df382dd244406176a3f57
qiongL910 <liuqiong891023@gmail.com>
2024-05-13 08:58:54 -0500

    added validationary tests

--- tests/test_validationry_refactored.py
+++ tests/test_validationry_refactored.py
@@ -4,6 +4,7 @@ import mock
 import pytest
 import pandas as pd
 from unittest.mock import MagicMock
+import numpy as np
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir)
@@ -11,6 +12,8 @@ from src.s3_validationry_refactored import (
     if_template_valid,
     if_string_float,
     if_string_int,
+    cleanup_manifest_nodes,
+    validate_required_properties_one_sheet
 )
 
 
@@ -49,7 +52,6 @@ def test_if_string_float():
     """
     pass_string = if_string_float("2.3")
     fail_again_string = if_string_float("test")
-    print(fail_again_string)
     assert pass_string
     assert fail_again_string is False
 
@@ -59,3 +61,59 @@ def test_if_string_int():
     fail_string = if_string_int("3.2")
     assert pass_string
     assert fail_string is False
+
+
+@mock.patch("src.s3_validationry_refactored.CheckCCDI")
+def test_cleanup_manifest_nodes(mock_checkccdi):
+    mock_file_object = MagicMock()
+    logger = MagicMock()
+    mock_checkccdi.return_value = mock_file_object
+    mock_file_object.get_sheetnames.return_value = [
+            "README and INSTRUCTIONS",
+            "apple",
+            "Dictionary",
+            "pear",
+            "Terms and Value Sets",
+            "grape"
+        ]
+    mock_file_object.read_sheet_na.side_effect = [
+        pd.DataFrame({
+            "type":[],
+            "apple_id":[]
+        }), 
+        pd.DataFrame({
+            "type":["pear","pear"],
+            "pear_id":["pear_1","pear_2"],
+            "pear_color": ["green","yellow"]
+        }), 
+        pd.DataFrame({
+            "type":["grape"]*3,
+            "grape_id":["grape_1","grape_2","grape_3"],
+            "grape_color": ["purple","purple","green"]
+        })
+    ]
+    template_node_list = ["grape","apple","pear"]
+    ordered_nodes = cleanup_manifest_nodes.fn(file_path="test_file_path", template_node_list=template_node_list, logger=logger)
+    assert ordered_nodes == ["grape","pear"]
+
+
+def test_validate_required_properties_one_sheet():
+    file_object=MagicMock()
+    test_df = pd.DataFrame(
+        {
+            "type": ["type"] * 25,
+            "required_prop1": ["anything"] * 8 + [np.nan] * 3 + ["anything"] * 14,
+            "required_prop2": ["anything"] * 21 + [np.nan] * 4,
+            "nonrequired_prop1": ["anything"] * 7 + [np.nan] * 3 + ["anything"] * 15
+        }
+    )
+    file_object.read_sheet_na.return_value = test_df
+    return_str = validate_required_properties_one_sheet.fn(
+        node_name="test_node",
+        checkccdi_object=file_object,
+        required_properties=["required_prop1", "required_prop2"],
+    )
+    assert "ERROR: The values for the node, test_node, in the the required property, required_prop1, are missing:" in return_str
+    assert "10,11,12" in return_str
+    assert "ERROR: The values for the node, test_node, in the the required property, required_prop2, are missing:" in return_str
+    assert "nonrequired_prop1" not in return_str
