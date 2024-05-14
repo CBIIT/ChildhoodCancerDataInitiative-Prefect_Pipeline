aafe092c5c90e99e1cd0fdd6904da00f5d95b788
qiongL910 <liuqiong891023@gmail.com>
2024-05-14 13:57:47 -0500

    added unittests for refactored validation script

--- tests/test_validationry_refactored.py
+++ tests/test_validationry_refactored.py
@@ -5,15 +5,28 @@ import pytest
 import pandas as pd
 from unittest.mock import MagicMock
 import numpy as np
+from pathlib import Path
+
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir)
+from src.utils import CheckCCDI
 from src.s3_validationry_refactored import (
     if_template_valid,
     if_string_float,
     if_string_int,
     cleanup_manifest_nodes,
-    validate_required_properties_one_sheet
+    validate_required_properties_one_sheet,
+    validate_whitespace_one_sheet,
+    validate_terms_value_sets_one_sheet,
+    validate_integer_numeric_checks_one_sheet,
+    validate_unique_key_one_sheet,
+    extract_object_file_meta,
+    check_file_size_zero,
+    check_file_md5sum_regex,
+    check_file_basename,
+    validate_cross_links_single_sheet,
+    validate_key_id_single_sheet,
 )
 
 
@@ -65,6 +78,7 @@ def test_if_string_int():
 
 @mock.patch("src.s3_validationry_refactored.CheckCCDI")
 def test_cleanup_manifest_nodes(mock_checkccdi):
+    """test for cleanup_manifest_nodes task"""
     mock_file_object = MagicMock()
     logger = MagicMock()
     mock_checkccdi.return_value = mock_file_object
@@ -98,6 +112,7 @@ def test_cleanup_manifest_nodes(mock_checkccdi):
 
 
 def test_validate_required_properties_one_sheet():
+    """test for validate_required_properties_one_sheet task"""
     file_object=MagicMock()
     test_df = pd.DataFrame(
         {
@@ -117,3 +132,196 @@ def test_validate_required_properties_one_sheet():
     assert "10,11,12" in return_str
     assert "required_prop2" in return_str
     assert "nonrequired_prop1" not in return_str
+
+
+def test_validate_whitespace_one_sheet():
+    """task for validate_whitespace_one_sheet"""
+    file_object = MagicMock()
+    test_df = pd.DataFrame(
+        {
+        "type": ["test_type"] * 3,
+        "prop1": ["apple","orange","pear   "],
+        "prop2": ["   milk","juice","wine"]
+        }
+    )
+    file_object.read_sheet_na.return_value = test_df
+    return_str = validate_whitespace_one_sheet.fn(node_name="test_node", checkccdi_object=file_object)
+    assert "prop1" in return_str
+    assert "prop2" in return_str
+    assert "4" in return_str
+
+
+# Dir containing 8 files
+FIXTURE_DIR = Path(__file__).parent.resolve() / "test_files"
+ALL_FILES = pytest.mark.datafiles(
+    FIXTURE_DIR / "CCDI_Submission_Template_v1.7.2.xlsx",
+    FIXTURE_DIR
+    / "CCDI_Submission_Template_v1.7.2_20Exampler_w_missing_value_additional_error.xlsx",
+)
+
+
+@ALL_FILES
+def test_validate_terms_value_sets_one_sheet(datafiles):
+    """ test for validate_terms_value_sets_one_sheet task"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    temp_object = CheckCCDI(temp_path)
+    file_object = CheckCCDI(file_path)
+    validate_str = validate_terms_value_sets_one_sheet.fn(
+        node_name="study_personnel",
+        checkccdi_object=file_object,
+        template_object=temp_object,
+    )
+    assert "free strings allowed" in validate_str
+    assert "personnel_type" in validate_str
+    assert "wasteful_fantastic_6" in validate_str
+    assert "institution" not in validate_str
+
+
+@ALL_FILES
+def test_validate_terms_value_sets_one_sheet_no_error(datafiles):
+    """ test for validate_terms_value_sets_one_sheet task"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    temp_object = CheckCCDI(temp_path)
+    file_object = CheckCCDI(file_path)
+    validate_str = validate_terms_value_sets_one_sheet.fn(
+        node_name="study_funding",
+        checkccdi_object=file_object,
+        template_object=temp_object,
+    )
+    assert "\n\tstudy_funding\n\t----------\n\t\n" == validate_str
+
+
+@ALL_FILES
+def test_validate_integer_numeric_checks_one_sheet(datafiles):
+    """test for validate_integer_numeric_checks_one_sheet task"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    temp_object = CheckCCDI(temp_path)
+    file_object = CheckCCDI(file_path)
+    validate_str = validate_integer_numeric_checks_one_sheet.fn(
+        node_name="single_cell_sequencing_file", file_object=file_object, template_object=temp_object
+    )
+    assert "coverage" in validate_str
+
+
+@ALL_FILES
+def test_validate_unique_key_one_sheet(datafiles):
+    """test for validate_integer_numeric_checks_one_sheet task"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    temp_object = CheckCCDI(temp_path)
+    file_object = CheckCCDI(file_path)
+    validate_str = validate_unique_key_one_sheet.fn(
+        node_name="study_personnel",
+        file_object=file_object,
+        template_object=temp_object,
+    )
+    assert "obeisant_pathetic_26" in validate_str
+
+
+@ALL_FILES
+def test_check_file_size_zero(datafiles):
+    """test for check_file_size_zero"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    file_object = CheckCCDI(file_path)
+    file_df = extract_object_file_meta(
+        nodes_list=[
+            "radiology_file",
+            "sequencing_file",
+        ],
+        file_object=file_object,
+    )
+    print_str = check_file_size_zero(file_df=file_df)
+    assert print_str.count("radiology_file") == 2
+    assert print_str.count("sequencing_file") == 1
+
+@ALL_FILES
+def test_check_file_md5sum_regex(datafiles):
+    """test for check_file_md5sum_regex"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    file_object = CheckCCDI(file_path)
+    file_df = extract_object_file_meta(
+        nodes_list=[
+            "pathology_file",
+            "single_cell_sequencing_file",
+        ],
+        file_object=file_object,
+    )
+    print_str = check_file_md5sum_regex(file_df=file_df)
+    assert print_str.count("pathology_file") == 1
+    assert "heartbreaking_whimsical_13" in print_str
+    assert "gkfjvhvjhf7" in print_str
+
+
+@ALL_FILES
+def test_check_file_basename(datafiles):
+    """test for check_file_basename"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    file_object = CheckCCDI(file_path)
+    file_df = extract_object_file_meta(
+        nodes_list=[
+            "single_cell_sequencing_file",
+        ],
+        file_object=file_object,
+    )
+    print_str = check_file_basename(file_df=file_df)
+    assert print_str.count("single_cell_sequencing_file") == 20
+
+
+@ALL_FILES
+def test_validate_cross_links_single_sheet(datafiles):
+    """test for validate_cross_links_single_sheet task"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    file_object = CheckCCDI(file_path)
+    print_str = validate_cross_links_single_sheet.fn(
+        node_name="single_cell_sequencing_file", file_object=file_object
+
+    )
+    assert "diligent_overwrought_80" in print_str
+    assert print_str.count("pdx.pdx_id") == 1
+
+@ALL_FILES
+def test_validate_key_id_single_sheet(datafiles):
+    """test for validate_key_id_single_sheet task"""
+    for item in datafiles.iterdir():
+        if "Exampler" in item.name:
+            file_path = str(item)
+        else:
+            temp_path = str(item)
+    file_object = CheckCCDI(file_path)
+    temp_object = CheckCCDI(temp_path)
+    print_str = validate_key_id_single_sheet.fn(
+        node_name="pdx", file_object=file_object, template_object=temp_object
+    )
+    assert print_str.count("pdx") == 2
+    assert "diligent_overwrought_80&" in print_str
