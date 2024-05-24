f18d4be0802b5b70641ce2cc86a1bfe3309aabf2
qiongL910 <liuqiong891023@gmail.com>
2024-05-24 12:06:17 -0500

    modified dcf indexing workflow which now can replace missing guid with new one

--- workflows/dcf_indexing.py
+++ workflows/dcf_indexing.py
@@ -2,6 +2,7 @@ from prefect import flow, get_run_logger
 import os
 import sys
 import pandas as pd
+from shutil import copy
 
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -13,6 +14,7 @@ from src.utils import (
     extract_dcf_index,
     combine_dcf_dicts,
     get_time,
+    get_date
 )
 
 
@@ -27,11 +29,17 @@ def dcf_index_manifest(
 
     # get time
     current_time = get_time()
+    # define output folder name in bucket for uploading
+    output_folder = os.path.join(runner, "dcf_index_output_" + current_time)
 
     logger.info(f"Downloading manifest {manifest_path} from bucket {bucket}")
     file_dl(bucket=bucket, filename=manifest_path)
     manifest_file = os.path.basename(manifest_path)
     manifest_obj = CheckCCDI(ccdi_manifest=manifest_file)
+    
+    # copy the manifest and rename for potential new guids assigned
+    modified_manifest_file = manifest_file.rsplit(".", 1)[0] + "_GUIDadded" + get_date() + ".xlsx"
+    copy(manifest_file, modified_manifest_file)
 
     # extract study accession of the manifest
     study_accession = manifest_obj.get_study_id()
@@ -46,7 +54,7 @@ def dcf_index_manifest(
     # extract columns related to dcf indexing
     logger.info("Extracting columns for DCF indexing")
     list_dicts = extract_dcf_index(
-        CCDI_manifest=manifest_obj, sheetname_list=obj_sheets
+        CCDI_manifest=manifest_obj, sheetname_list=obj_sheets, modified_manifest=modified_manifest_file
     )
     # combined these dicts into a single dict
     combined_dict = combine_dcf_dicts(list_dicts=list_dicts)
@@ -55,9 +63,29 @@ def dcf_index_manifest(
     # Convert combined_dict into a dataframe
     # add acl and authz
     combined_df = pd.DataFrame(combined_dict)
+    del combined_dict
     logger.info(f"Number of objects in total: {combined_df.shape[0]}")
+    if sum(combined_df["if_guid_missing"])>0:
+        # upload modified_manifest_file only if missing guid objects were found
+        counts_of_missing_guid = sum(combined_df["if_guid_missing"])
+        missing_guid_summary_df = combined_df[combined_df["if_guid_missing"]].groupby(["node"]).size().reset_index(name="counts")
+        missing_guid_summary_str = missing_guid_summary_df.to_markdown(tablefmt="pipe", index=False)
+        logger.warning(f"Number of objects with missing guid were assigned with new one: {counts_of_missing_guid}\n{missing_guid_summary_str}")
+        file_ul(
+            bucket=bucket,
+            output_folder=output_folder,
+            sub_folder="",
+            newfile=modified_manifest_file
+        )
+        logger.info(f"Uploaded modified manifest {modified_manifest_file} with new guids to bucket {bucket} folder path {output_folder}")
+        del missing_guid_summary_df
+    else:
+        pass
     combined_df.drop_duplicates(inplace=True, ignore_index=True)
     logger.info(f"Number of objects in total after removing duplicates: {combined_df.shape[0]}")
+
+    # finish up with the dcf index df
+    combined_df.drop(columns=["node","if_guid_missing"], inplace=True)
     combined_df["acl"] = acl
     combined_df["authz"] = authz
     col_order = ["GUID", "md5", "size", "acl", "authz", "urls"]
@@ -68,7 +96,6 @@ def dcf_index_manifest(
     combined_df.to_csv(output_filename, sep="\t", index=False)
     del combined_df
     # upload file to the bucket
-    output_folder = os.path.join(runner, "dcf_index_output_" + current_time)
     file_ul(
         bucket=bucket,
         output_folder=output_folder,
@@ -76,7 +103,7 @@ def dcf_index_manifest(
         newfile=output_filename,
     )
     logger.info(
-        f"Uploaded output {output_filename} to the bucket {bucket} folder {output_folder}"
+        f"Uploaded output {output_filename} to bucket {bucket} folder path {output_folder}"
     )
     logger.info("Workflow finished!")
 
