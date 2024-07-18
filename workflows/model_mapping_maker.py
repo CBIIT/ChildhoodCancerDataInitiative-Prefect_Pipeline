5d518fcb7c7911968f876a9d63202d686da0e908
svburke <sean.burke2@nih.gov>
2024-07-18 13:13:31 -0500

    Update model_mapping_maker.py

--- workflows/model_mapping_maker.py
+++ workflows/model_mapping_maker.py
@@ -655,16 +655,16 @@ def runner(
     )
 
     comparison_df_out = comparison_df
-    # Change column names for prefect script
-    comparison_df_out.columns = [
-        "state",
-        "lift_from_node",
-        "lift_from_property",
-        "lift_from_version",
-        "lift_to_node",
-        "lift_to_property",
-        "lift_to_version",
-    ]
+    # # Change column names for prefect script
+    # comparison_df_out.columns = [
+    #     "state",
+    #     "lift_from_node",
+    #     "lift_from_property",
+    #     "lift_from_version",
+    #     "lift_to_node",
+    #     "lift_to_property",
+    #     "lift_to_version",
+    # ]
 
     comparison_df_out.to_csv(
         comparison_mapping_file_name,
