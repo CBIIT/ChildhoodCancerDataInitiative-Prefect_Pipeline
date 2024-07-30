58879dbe259df54be95bb9bff0a986e4d4f04b7c
qiongL910 <liuqiong891023@gmail.com>
2024-07-30 12:28:25 -0500

    try a fix

--- src/s3_validationry_refactored.py
+++ src/s3_validationry_refactored.py
@@ -737,6 +737,7 @@ def validate_unique_key_one_sheet(node_name: str, file_object, template_object):
     print(dict_df)
     print(dict_df.columns)
     print(dict_df[dict_df["Node"]==node_name][["Property", "Node", "Required", "Key"]])
+    print(dict_df[dict_df["Node"] == node_name]["Key"].dropna().tolist())
     # pull out all key value properties
     key_value_props = dict_df.loc[
         (dict_df["Key"] == True) & (dict_df["Node"] == node_name), "Property"
