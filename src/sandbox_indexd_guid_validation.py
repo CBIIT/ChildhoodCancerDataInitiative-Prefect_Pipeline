c9420295f9eb9449e369cfb1a9b0167fc2cf1a18
qiongL910 <liuqiong891023@gmail.com>
2024-10-16 11:14:05 -0400

    combine csv files into a single tsv

--- src/sandbox_indexd_guid_validation.py
+++ src/sandbox_indexd_guid_validation.py
@@ -1,14 +1,15 @@
-from utils import CCDI_Tags, CheckCCDI, get_time, get_date, folder_ul
+from utils import CCDI_Tags, CheckCCDI, get_time, get_date, folder_ul, file_ul
 from neo4j_data_tools import export_to_csv, pull_data_per_node, cypher_query_parameters
 from prefect import flow, task, get_run_logger
 from prefect.task_runners import ConcurrentTaskRunner
 from neo4j import GraphDatabase
 import os
 from pathlib import Path
+import pandas as pd
 
 
 @flow(task_runner=ConcurrentTaskRunner())
-def pull_study_nodes_loop(study_accession: str, node_list: list, driver, out_dir: str, logger) -> None:
+def pull_guid_meta_nodes_loop(study_accession: str, node_list: list, driver, out_dir: str, logger) -> None:
     """Loops through a list of node labels and pulls data of a given study from a neo4j DB"""
     phs_accession =  study_accession
     guid_meta_query =  f"""
@@ -27,6 +28,31 @@ RETURN f.dcf_indexd_guid as guid, f.acl as acl, f.file_url as url, f.md5sum as m
             output_dir=out_dir,
         )
     return None
+
+
+@task
+def concatenate_csv_files(folder_name: str) -> str:
+    """Merge all csv files of guid meta from one study
+
+    Args:
+        folder_name (str): folder name containg several csv files of guid meta data
+
+    Returns:
+        str: output filename
+    """    
+    file_list = [os.path.join(folder_name, i) for i in os.listdir(folder_name)]
+    
+    for index in range(len(file_list)):
+        if index == 0:
+            combined_df =  pd.read_csv(file_list[index])
+        else:
+            file_df = pd.read_csv(file_list[index])
+            combined_df = pd.concat([combined_df, file_df], ignore_index=True)
+    phs_accession = folder_name.split("_")[0]
+    output_name = phs_accession + "_guid_meta_in_sandbox.tsv"
+    combined_df.to_csv(output_name, sep="\t", index=False)
+    return output_name
+
 
 @flow
 def query_guid_meta_sandbox(phs_accession: str, data_model_tag: str, bucket: str, runner: str) -> None:
@@ -63,7 +89,7 @@ def query_guid_meta_sandbox(phs_accession: str, data_model_tag: str, bucket: str
     foldername = phs_accession + "_guid_csv"
     # create foldername folder if not exist
     Path(foldername).mkdir(parents=True, exist_ok=True)
-    pull_study_nodes_loop(
+    pull_guid_meta_nodes_loop(
         study_accession=phs_accession,
         node_list=file_nodes,
         driver=driver,
@@ -71,6 +97,15 @@ def query_guid_meta_sandbox(phs_accession: str, data_model_tag: str, bucket: str
         logger=logger
     )
 
+    # combined all sandbox guid metadata into one tsv
+    tsv_guid_meta = concatenate_csv_files(folder_name=foldername)
+
     # upload the folder to s3 bucket
     output_folder = os.path.join(runner,"sandbox_guid_pull_" + current_time)
-    folder_ul(local_folder=foldername, bucket=bucket, destination=output_folder, sub_folder="")
+    # folder_ul(local_folder=foldername, bucket=bucket, destination=output_folder, sub_folder="")
+    file_ul(
+        bucket=bucket, output_folder=output_folder, sub_folder="", newfile=tsv_guid_meta
+    )
+    
+    # check sandbox guid against indexd record
+    
