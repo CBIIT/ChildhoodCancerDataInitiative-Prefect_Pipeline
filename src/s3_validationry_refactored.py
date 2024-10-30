8a3ae38e7354997661b2ef35e211fa8a09200931
qiongL910 <liuqiong891023@gmail.com>
2024-10-30 15:44:40 -0400

    added unique guid validation

--- src/s3_validationry_refactored.py
+++ src/s3_validationry_refactored.py
@@ -1293,6 +1293,72 @@ def validate_file_metadata(
     return None
 
 
+@flow(name="Validate unique guid for each url", log_prints=True)
+def validate_unique_guid(
+    node_list: list[str], file_path: str, template_path: str, output_file: str
+) -> None:
+    """Validates if guid is unique for every url
+
+    Args:
+        node_list (list[str]): a list of node names
+        file_path (str): file path of manifest
+        template_path (str): file path of template
+        output_file (str): output file name
+    """
+    section_title = (
+        "\n\n"
+        + header_str("Unique GUID Validation")
+        + "\nThe following section will check if every unique guid is only responsible for one url.\nIf there are any unexpected values, they will be reported below:\n----------\n"
+    )
+    return_str = "" + section_title
+    # create file_object and template_object
+    template_object = CheckCCDI(ccdi_manifest=template_path)
+    file_object = CheckCCDI(ccdi_manifest=file_path)
+
+    # read dict_df
+    dict_df = template_object.read_sheet_na(sheetname="Dictionary")
+    file_nodes = dict_df[dict_df["Property"] == "file_url"]["Node"].values.tolist()
+    file_nodes_to_check = [i for i in node_list if i in file_nodes]
+
+    guid_list = []
+    for node in file_nodes_to_check:
+        node_df = file_object.read_sheet_na(sheetname=node)
+        for index, row in node_df.iterrows():
+            row_node = node
+            row_guid = row["dcf_indexd_guid"]
+            row_url = row["file_url"]
+            guid_list.append([row_node, row_guid, row_url])
+    guid_df = pd.DataFrame(guid_list, columns=["node", "dcf_indexd_guid", "file_url"])
+    guid_df.drop_duplicates(subset=["dcf_indexd_guid", "file_url"], inplace=True)
+
+    # find guid responsible for more than one url
+    error_guid_df = (
+        guid_df.groupby("dcf_indexd_guid")
+        .filter(lambda x: len(x) > 1)
+        .reset_index(drop=True)
+    )
+    if error_guid_df.shape[0] == 0:
+        return_str = (
+            return_str
+            + f"\tPASS: Every indexd guid is responsible for only one url.\n"
+        )
+    else:
+        return_str = (
+            return_str
+            + "\n\t"
+            + error_guid_df.to_markdown(tablefmt="rounded_grid", index=False).replace(
+                "\n", "\n\t"
+            )
+            + "\n\n"
+        )
+
+    # print the return_str to output_file
+    with open(output_file, "a+") as outf:
+        outf.write(return_str)
+    
+    return None
+
+
 @flow(name="Validate AWS bucket content", log_prints=True)
 def validate_bucket_content(
     node_list: list[str], file_path: str, template_path: str, output_file: str
