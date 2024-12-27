db44d911fdc6f7fc68fd729cacb59b5002b4e81d
bullenc <75388530+bullenc@users.noreply.github.com>
2024-12-27 14:41:22 -0600

    Update vcf_merge.py

--- workflows/vcf_merge.py
+++ workflows/vcf_merge.py
@@ -119,7 +119,7 @@ def merging(df: pd.DataFrame):
             #change column names to temp sample names >>> TODO: need actual sample names? where to find?
             vcf_df = vcf_df.rename(columns={"tumor" : "tumor"+row["patient_id"], "normal" : "normal"+row["patient_id"]})
 
-            runner_logger.info(vcf_df)
+            print(vcf_df)
             
             #append to list of vcf_dfs
             vcf_dfs.append(vcf_df)
