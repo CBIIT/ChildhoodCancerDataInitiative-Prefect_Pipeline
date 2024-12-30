5cc3909ce7e7eccbbc9ac2a49ed0a614ed9d0251
bullenc <75388530+bullenc@users.noreply.github.com>
2024-12-30 10:18:08 -0600

    Update gdc_file_upload.py

--- workflows/gdc_file_upload.py
+++ workflows/gdc_file_upload.py
@@ -95,7 +95,7 @@ def retrieve_s3_url(rows: pd.DataFrame):
             for record in json.loads(response.text)["records"]:
                 for url in record["urls"]:
                     if url != "":
-                        s3_url = ""
+                        s3_url = url
 
             if s3_url == "":
                 runner_logger.error(
