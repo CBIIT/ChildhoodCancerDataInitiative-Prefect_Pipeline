0c417e950d04a0d436b2fa563f0f7b2c7f96cb64
bullenc <75388530+bullenc@users.noreply.github.com>
2025-01-02 14:42:19 -0600

    Update gdc_file_upload.py

--- workflows/gdc_file_upload.py
+++ workflows/gdc_file_upload.py
@@ -194,7 +194,7 @@ def upload_request_chunks(
     program = project_id.split("-")[0]
     project = "-".join(project_id.split("-")[1:])
 
-    chunk_size = 5 * 1024 * 1024 #5MB
+    chunk_size = 2 * 1024 * 1024 #5MB
     max_threads = 2
 
     file_size = os.path.getsize(f_name)
