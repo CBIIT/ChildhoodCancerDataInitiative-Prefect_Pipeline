e20fcc58646f964f2e398220073013778bc11122
bullenc <75388530+bullenc@users.noreply.github.com>
2025-01-07 14:50:33 -0600

    Add in 'recursive' merging

--- workflows/vcf_merge.py
+++ workflows/vcf_merge.py
@@ -9,6 +9,7 @@ import sys
 import pandas as pd
 from fuc import pyvcf
 from datetime import datetime
+from typing import Literal
 
 
 # prefect dependencies
@@ -140,12 +141,48 @@ def merging(df: pd.DataFrame):
             #runner_logger.error("Issue merging VCF files.")
             #merged_vcf = ""
     else:
-        runner_logger.error("No VCF files provided to merge. Exiting process.")
-        sys.exit(1)
+        runner_logger.error("No VCF files provided to merge.")
+        merged_vcf = ""
 
 
     return vcf_to_merge, not_merged, merged_vcf
 
+def merging_merged(merged_vcfs: list):
+    """Function to call fuc libray to merge a set of previously merged VCFs
+    """
+
+    runner_logger = get_run_logger()
+
+    vcf_to_merge = []
+    not_merged = []
+
+    for vcf in merged_vcfs:
+        if os.path.exists(vcf):
+            vcf_to_merge.append(vcf)
+        else:
+            not_merged.append(vcf)
+
+    if len(vcf_to_merge) > 1:
+        runner_logger.info(f"Running merge of {len(vcf_to_merge)} previously merged VCF files....")
+        
+        try:
+            vcf_dfs = [] #list to store VCF DFs
+            for vcf in vcf_to_merge:
+                #read in VCF
+                vcf_df = pyvcf.VcfFrame.from_file(vcf)
+                vcf_dfs.append(vcf_df)
+            
+            merged_vcf = pyvcf.merge(vcf_dfs, how='outer').filter_multialt().df
+            runner_logger.info("Merge Complete!")
+        except Exception as e:
+            runner_logger.error(f"Exception occurred: {e}")
+            merged_vcf = ""
+    else:
+        runner_logger.error("No VCF files provided to merge.")
+        merged_vcf = ""
+    
+    return vcf_to_merge, not_merged, merged_vcf
+        
 
 @flow(
     name="vcf_merge_remove_temp_vcfs",
@@ -167,6 +204,7 @@ def delete_handler(df: pd.DataFrame):
         
     return None
 
+DropDownChoices = Literal["yes", "no"]
 
 @flow(
     name="VCF Merge",
@@ -177,6 +215,7 @@ def runner(bucket: str,
     manifest_path: str,
     runner: str,
     chunk_size: int,
+    recursive: DropDownChoices
 ):
     """CCDI Pipeline to Upload files to GDC
 
@@ -185,6 +224,7 @@ def runner(bucket: str,
         manifest_path (str): File path of the CCDI file manifest in bucket
         runner (str): Unique runner name
         chunk_size (str): Integer for the number of files that should be merged at a time
+        recursive (str): Whether to perform recursive merging of provided list of VCFs into one VCF
     """
     
     runner_logger = get_run_logger()
@@ -202,6 +242,8 @@ def runner(bucket: str,
     os.mkdir(f"VCF_merge_{chunk_size}_{dt}")
 
     runner_logger.info(">>> Running VCF_MERGE.py ....")
+
+    first_pass_vcfs = [] #record first pass merged VCFs
 
     for chunk in range(0, len(file_metadata), chunk_size):
         runner_logger.info(f"Working on chunk {round(chunk/chunk_size)+1} of {len(range(0, len(file_metadata), chunk_size))}...")
@@ -226,11 +268,31 @@ def runner(bucket: str,
             w.close()
 
         merged_vcf.to_csv(f"VCF_merge_{chunk_size}_{dt}/merged_file_{chunk}.vcf", sep="\t", index=False)
-
-
+        first_pass_vcfs.append(f"VCF_merge_{chunk_size}_{dt}/merged_file_{chunk}.vcf")
+    
         # delete VCFs
         runner_logger.info(f"Removing constituent VCFs in chunk {round(chunk/chunk_size)+1}")
         delete_handler(file_metadata[chunk:chunk+chunk_size])
+
+    if recursive == 'yes':
+        runner_logger.info("Merging the first pass merged VCFs together...")
+        vcf_to_merge_p2, not_merged_p2, merged_vcf_p2 = merging_merged(first_pass_vcfs)
+
+        # save merged file and log files
+        runner_logger.info(f"Saving doubly merged VCF and info")
+        with open(f"VCF_merge_{chunk_size}_{dt}/vcfs_chunks_merged_together.txt", "w+") as w:
+            w.write("\n".join(vcf_to_merge_p2))
+        w.close()
+
+        if len(not_merged) > 0:
+            with open(f"VCF_merge_{chunk_size}_{dt}/vcfs_chunks_NOT_merged_together.txt", "w+") as w:
+                w.write("\n".join(not_merged_p2))
+            w.close()
+
+        merged_vcf_p2.to_csv(f"VCF_merge_{chunk_size}_{dt}/complete_merged_file_all_chunks.vcf", sep="\t", index=False)
+
+    else:
+        runner_logger.info("Chunks of VCFs merged finished, but chunks not merged together.")
 
 
     # dl folder to somewhere else
