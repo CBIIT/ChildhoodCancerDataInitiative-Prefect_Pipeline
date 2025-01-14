58008e8dd0517371eada11bc7aaf3c58666342e8
bullenc <75388530+bullenc@users.noreply.github.com>
2025-01-14 15:08:42 -0600

    Update vcf_merge_bcftools.py

--- workflows/vcf_merge_bcftools.py
+++ workflows/vcf_merge_bcftools.py
@@ -113,7 +113,7 @@ def bcftools_install(bucket: str, file_path: str):
 
     runner_logger.info(f"apt update results: OUT: {std_out}, ERR: {std_err}")
 
-    for package in ["libz-dev", "liblzma-dev", "bzip2"]:
+    for package in ["libz-dev", "liblzma-dev", "libbz2-1.0"]:
         process = subprocess.Popen(["apt", "install", package], shell=False, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,)
         std_out, std_err = process.communicate()
         runner_logger.info(f"apt intstall {package} results: OUT: {std_out}, ERR: {std_err}")
