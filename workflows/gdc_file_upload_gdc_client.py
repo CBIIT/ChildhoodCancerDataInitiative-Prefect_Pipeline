cef35c5f53083f5f332f527175c105d259e586f1
bullenc <75388530+bullenc@users.noreply.github.com>
2025-01-16 13:32:56 -0600

    Update gdc_file_upload_gdc_client.py

--- workflows/gdc_file_upload_gdc_client.py
+++ workflows/gdc_file_upload_gdc_client.py
@@ -371,7 +371,7 @@ def runner(
 
     runner_logger.info(f"Trying GET to https://api.gdc.cancer.gov/v0/submission/CCDI/MCI/entities/005ebef7-c384-433a-bea9-91afdb332ecb")
 
-    runner_logger.info(requests.get("https://api.gdc.cancer.gov/v0/submission/CCDI/MCI/entities/005ebef7-c384-433a-bea9-91afdb332ecb", headers={"X-Auth-Token": token}).text)
+    runner_logger.info(requests.get("https://api.gdc.cancer.gov/v0/submission/CCDI/MCI/entities/005ebef7-c384-433a-bea9-91afdb332ecb").text)
 
     runner_logger.info(f"Trying GET to https://api.gdc.cancer.gov/cases?size=1")
     
