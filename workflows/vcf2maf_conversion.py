d475dc7d37c5daf2852a11fce3fa1b8d0075addf
bullenc <75388530+bullenc@users.noreply.github.com>
2025-01-28 14:22:59 -0600

    Update vcf2maf_conversion.py

--- workflows/vcf2maf_conversion.py
+++ workflows/vcf2maf_conversion.py
@@ -33,7 +33,15 @@ def env_setup():
 
     runner_logger.info(ShellOperation(commands=[
         "apt update",
-        "apt-get -y install curl miniconda"
+        "apt-get -y install curl",
+        "curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg",
+        "install -o root -g root -m 644 conda.gpg /usr/share/keyrings/conda-archive-keyring.gpg",
+        # Check whether fingerprint is correct (will output an error message otherwise)
+        "gpg --keyring /usr/share/keyrings/conda-archive-keyring.gpg --no-default-keyring --fingerprint 34161F5BF5EB1D4BFBBB8F0A8AEB4F8B29D82806",
+        # Add our Debian repo
+        "echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main\" > /etc/apt/sources.list.d/conda.list",
+        "apt update",
+        "apt -y install conda"
     ]).run())
 
 
