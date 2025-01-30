216c094fd638a9b5b418b6e0c9cce0c688565fa6
bullenc <75388530+bullenc@users.noreply.github.com>
2025-01-30 07:54:58 -0600

    Update vcf2maf_conversion.py

--- workflows/vcf2maf_conversion.py
+++ workflows/vcf2maf_conversion.py
@@ -326,7 +326,7 @@ def runner(
     #env_check()
     #vep_setup()
     runner_logger.info(ShellOperation(commands=[
-        "ls -lh /usr/local/data"
+        "ls -lh /usr/local/data/vep"
     ]).run())
 
     #bwa_setup(bucket, bwa_tarball_path)
