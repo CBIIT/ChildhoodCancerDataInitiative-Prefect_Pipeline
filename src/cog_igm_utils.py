9dbdd47fd725248846c7a661ed7e143229fb965d
bullenc <75388530+bullenc@users.noreply.github.com>
2025-03-20 12:06:49 -0500

    Update cog_igm_utils.py

--- src/cog_igm_utils.py
+++ src/cog_igm_utils.py
@@ -271,7 +271,7 @@ def cog_igm_json2tsv(
         if parsing in ["cog_only", "cog_and_igm"]:
             if len(df_reshape) > 0:
                 cog_form_parser(df_reshape, dt, cog_op, logger)
-                cog_transformer(df_reshape_file_name, cog_op)
+                #cog_transformer(df_reshape_file_name, cog_op) ##testing
             else:
                 logger.error(
                     "Cannot perform COG form-level parsing, no valid COG JSONs read in."
