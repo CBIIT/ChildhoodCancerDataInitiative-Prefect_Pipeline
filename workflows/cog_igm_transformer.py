212a73c4faa19a9b8352b6e59e7bac8edcac6272
bullenc <75388530+bullenc@users.noreply.github.com>
2025-03-20 11:39:49 -0500

    Update cog_igm_transformer.py

--- workflows/cog_igm_transformer.py
+++ workflows/cog_igm_transformer.py
@@ -144,6 +144,7 @@ def cog_igm_transform(
     runner_logger.info(
         ShellOperation(
             commands=[
+                f"rm -r /usr/local/data/{output_path}/*",
                 f"rm -r /usr/local/data/{output_path}*",
                 f"rm -r /usr/local/data/{working_path}*",
                 f"ls -l /usr/local/data/",  # confirm removal of COG_IGM_Transform working dirs
