325f6919eb6b2f797db620737156ac430b0652d2
Cole DeVries <colemandevries@gmail.com>
2025-05-22 14:41:15 -0400

    flattened pydantic model

--- kids-first-main.py
+++ kids-first-main.py
@@ -4,49 +4,13 @@ from typing import Any, Dict, List
 
 import boto3
 from botocore.exceptions import ClientError
-
+from prefect import flow, get_run_logger, task
 from pydantic import BaseModel, Field
-from prefect import flow, task, get_run_logger
 
 logger = get_run_logger()
 session = boto3.Session()
-client = session.client('s3')
+client = session.client("s3")
 
-class Manifest(BaseModel):
-    bucket: str = Field(
-        title="Manifest Bucket Name",
-        description="The name of the S3 bucket where the manifest is stored",
-        default="ccdi-kidsfirst-transfer-manifests"
-    )
-
-    key: str = Field(
-        title="Manifest Key",
-        description="The S3 key for the manifest file",
-    )
-
-    url_column: str = Field(
-        title="Manifest URL Column",
-        description="The name of the column in the manifest that contains the S3 URLs",
-        default="urls"
-    )
-
-    status_column: str = Field(
-        title="Manifest Status Column",
-        description="The name of the column in the manifest that contains the object status",
-        default="registration_status"
-    )
-
-class NciDataBucket(BaseModel):
-    bucket: str = Field(
-        title="NCI Data Bucket Name",
-        description="The name of the S3 Bucket where CHOP data was transferred to",
-    )
-
-    suffix: str = Field(
-        title="NCI Data Bucket Suffix",
-        description="The suffix of the S3 bucket where CHOP data was transferred to",
-        default="nci"
-    )
 
 class StatusMapValues(BaseModel):
     kf_registered: bool = Field(
@@ -61,81 +25,106 @@ class StatusMapValues(BaseModel):
 
 
 class Config(BaseModel):
-    manifest: Manifest = Field(
-        title="Manifest Configuration",
-        description="Configuration parameters for the manifest file",
+
+    manifest_bucket: str = Field(
+        title="Manifest Bucket Name",
+        description="The name of the S3 bucket where the manifest is stored",
+        default="ccdi-kidsfirst-transfer-manifests",
     )
 
-    nci_data_bucket: NciDataBucket = Field(
-        title="NCI Data Bucket Configuration",
-        description="Configuration parameters for the NCI data bucket where CHOP data was transferred to",
+    manifest_key: str = Field(
+        title="Manifest Key",
+        description="The S3 key for the manifest file",
     )
 
-    status_map: List[Dict[str, StatusMapValues]] = Field(
+    manifest_url_column: str = Field(
+        title="Manifest URL Column",
+        description="The name of the column in the manifest that contains the S3 URLs",
+        default="urls",
+    )
+
+    manifest_status_column: str = Field(
+        title="Manifest Status Column",
+        description="The name of the column in the manifest that contains the object status",
+        default="registration_status",
+    )
+
+    nci_bucket: str = Field(
+        title="NCI Data Bucket Name",
+        description="The name of the S3 Bucket where CHOP data was transferred to",
+    )
+
+    nci_bucket_suffix: str = Field(
+        title="NCI Data Bucket Suffix",
+        description="The suffix of the S3 bucket where CHOP data was transferred to",
+        default="nci",
+    )
+
+    status_map: List[Dict[str, Dict[str, bool]]] = Field(
         title="Release and Registration Status Map Lookup",
         description="Mapping of status values to Kids First registered and released status",
         default=[
+            {"registered and released": {"kf_registered": True, "kf_released": True}},
+            {"released and registered": {"kf_registered": True, "kf_released": True}},
             {
-                "registered and released": StatusMapValues(
-                    kf_registered=True, kf_released=True
-                )
+                "registered and not released": {
+                    "kf_registered": True,
+                    "kf_released": False,
+                }
             },
             {
-                "released and registered": StatusMapValues(
-                    kf_registered=True, kf_released=True
-                )
+                "not registered and not released": {
+                    "kf_registered": False,
+                    "kf_released": False,
+                }
             },
             {
-                "registered and not released": StatusMapValues(
-                    kf_registered=True, kf_released=False
-                )
+                "not registered and released": {
+                    "kf_registered": False,
+                    "kf_released": True,
+                }
             },
             {
-                "not registered and not releeased": StatusMapValues(
-                    kf_registered=False, kf_released=False
-                )
+                "released and not registered": {
+                    "kf_registered": False,
+                    "kf_released": True,
+                }
             },
             {
-                "not registered and not released": StatusMapValues(
-                    kf_registered=False, kf_released=False
-                )
+                "not registered and not releeased": {
+                    "kf_registered": False,
+                    "kf_released": False,
+                }
             },
-            {
-                "not registered and released": StatusMapValues(
-                    kf_registered=False, kf_released=True
-                )
-            },
-            {
-                "released and not registered": StatusMapValues(
-                    kf_registered=False, kf_released=True
-                )
-            }
         ],
-    
     )
 
-@task
-def load_manifest(client: boto3.client, bucket: str, key: str) -> List[Dict[str, Any]]:
-    try:
-        result = client.get_object(Bucket=bucket, Key=key)
-        manifest = result['Body'].read().decode('utf-8').splitlines()
-        reader = csv.DictReader(manifest)
-        return [row for row in reader]
-    except ClientError as e:
-        raise
 
 @task
-def parse_manifest_url(manifest: List[Dict[str, Any]], manifest_url_column: str) -> List[Dict[str, Any]]:
+def load_manifest(s3_client: Any, bucket: str, key: str) -> List[Dict[str, Any]]:
+    try:
+        result = s3_client.get_object(Bucket=bucket, Key=key)
+        manifest = result["Body"].read().decode("utf-8").splitlines()
+        reader = csv.DictReader(manifest)
+        return [row for row in reader]
+    except ClientError as err:
+        raise err
+
+
+@task
+def parse_manifest_url(
+    manifest: List[Dict[str, Any]], manifest_url_column: str
+) -> List[Dict[str, Any]]:
     response: List[Dict[str, Any]] = []
-    
-    for row in manifest: 
+
+    for row in manifest:
         if not row[manifest_url_column]:
             row["chop_bucket"] = None
             row["chop_key"] = None
             row["valid_url"] = False
             row["invalid_url_reason"] = "No file URL detected"
             response.append(row)
-        
+
         if not row[manifest_url_column].startswith("s3://"):
             row["chop_bucket"] = None
             row["chop_key"] = None
@@ -160,8 +149,11 @@ def parse_manifest_url(manifest: List[Dict[str, Any]], manifest_url_column: str)
             response.append(row)
     return response
 
+
 @task
-def validate_manifest_bucket_name(manifest: List[Dict[str, Any]], nci_data_bucket: str, nci_data_bucket_suffix: str) -> List[Dict[str, Any]]:
+def validate_manifest_bucket_name(
+    manifest: List[Dict[str, Any]], nci_data_bucket: str, nci_data_bucket_suffix: str
+) -> List[Dict[str, Any]]:
     response: List[Dict[str, Any]] = []
     for row in manifest:
         if f'{row["chop_bucket"]}-{nci_data_bucket_suffix}' == nci_data_bucket:
@@ -171,8 +163,13 @@ def validate_manifest_bucket_name(manifest: List[Dict[str, Any]], nci_data_bucke
         response.append(row)
     return response
 
+
 @task
-def parse_object_status(manifest: List[Dict[str, Any]], manifest_status_column: str, status_map: List[Dict[str, Dict[str, bool]]]) -> List[Dict[str, Any]]:
+def parse_object_status(
+    manifest: List[Dict[str, Any]],
+    manifest_status_column: str,
+    status_map: List[Dict[str, Dict[str, bool]]],
+) -> List[Dict[str, Any]]:
     response: List[Dict[str, Any]] = []
     for row in manifest:
         if not row[manifest_status_column]:
@@ -180,7 +177,7 @@ def parse_object_status(manifest: List[Dict[str, Any]], manifest_status_column: 
             row["kf_released"] = None
             row["kf_status_valid"] = False
             row["invalid_status_reason"] = "No status column detected"
-        
+
         if row[manifest_status_column] not in status_map:
             row["kf_registered"] = None
             row["kf_released"] = None
@@ -190,33 +187,39 @@ def parse_object_status(manifest: List[Dict[str, Any]], manifest_status_column: 
         else:
             for status in status_map:
                 if row[manifest_status_column] in status:
-                    row["kf_registered"] = status[row[manifest_status_column]].kf_registered
-                    row["kf_released"] = status[row[manifest_status_column]].kf_released
+                    row["kf_registered"] = status[row[manifest_status_column]][
+                        "kf_registered"
+                    ]
+                    row["kf_released"] = status[row[manifest_status_column]][
+                        "kf_released"
+                    ]
                     row["kf_status_valid"] = True
                     row["invalid_status_reason"] = None
         response.append(row)
     return response
 
+
 @task
-def upload_object(client: boto3.client, bucket: str, key: str, manifest: List[Dict[str, Any]]) -> bool:
+def upload_object(
+    s3_client: Any, bucket: str, key: str, manifest: List[Dict[str, Any]]
+) -> bool:
     buffer = StringIO()
-    writer = csv.DictWriter(buffer, fieldnames=manifest[0].keys()) 
+    writer = csv.DictWriter(buffer, fieldnames=manifest[0].keys())
     writer.writeheader()
     writer.writerows(manifest)
-    response = client.put_object(
-        Bucket=bucket,
-        Key=key,
-        Body=buffer.getvalue(),
-        ContentType='text/csv'
+    response = s3_client.put_object(
+        Bucket=bucket, Key=key, Body=buffer.getvalue(), ContentType="text/csv"
     )
-    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
         return True
     else:
         return False
 
-        
-@task 
-def tag_objects(client: boto3.client, manifest: List[Dict[str, Any]], nci_data_bucket: str):
+
+@task
+def tag_objects(
+    s3_client: Any, manifest: List[Dict[str, Any]], nci_data_bucket: str
+) -> List[Dict[str, Any]]:
     response: List[Dict[str, Any]] = []
     for row in manifest:
         cond1 = row["valid_url"]
@@ -225,23 +228,20 @@ def tag_objects(client: boto3.client, manifest: List[Dict[str, Any]], nci_data_b
 
         if cond1 and cond2 and cond3:
             try:
-                response = client.put_object_tagging(
+                result = s3_client.put_object_tagging(
                     Bucket=nci_data_bucket,
                     Key=row["chop_key"],
                     Tagging={
-                        'TagSet': [
+                        "TagSet": [
                             {
-                                'Key': 'kf_registered',
-                                'Value': str(row["kf_registered"])
+                                "Key": "kf_registered",
+                                "Value": str(row["kf_registered"]),
                             },
-                            {
-                                'Key': 'kf_released',
-                                'Value': str(row["kf_released"])
-                            }
+                            {"Key": "kf_released", "Value": str(row["kf_released"])},
                         ]
-                    }
+                    },
                 )
-                if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                if result["ResponseMetadata"]["HTTPStatusCode"] == 200:
                     row["tagged"] = True
                 else:
                     row["tagged"] = False
@@ -260,23 +260,27 @@ def main(config: Config):
 
     logger.info("Starting Kids First Object Tagger flow")
     logger.info("Loading manifest from S3")
-    manifest1 = load_manifest(client, config.manifest.bucket, config.manifest.key)
+    manifest1 = load_manifest(client, config.manifest_bucket, config.manifest_key)
 
     logger.info("Parsing manifest URL column")
-    manifest2 = parse_manifest_url(manifest1, config.manifest.url_column)
+    manifest2 = parse_manifest_url(manifest1, config.manifest_url_column)
 
     logger.info("Validating bucket name parsed from the URL column")
-    manifest3 = validate_manifest_bucket_name(manifest2, config.nci_data_bucket.bucket, config.nci_data_bucket.suffix)
-    
+    manifest3 = validate_manifest_bucket_name(
+        manifest2, config.nci_bucket, config.nci_bucket_suffix
+    )
+
     logger.info("Parsing object status column")
-    manifest4 = parse_object_status(manifest3, config.manifest.status_column, config.status_map)
+    manifest4 = parse_object_status(
+        manifest3, config.manifest_status_column, config.status_map
+    )
 
     logger.info("Uploading manifest to S3")
     upload_enriched_manifest = upload_object(
         client,
-        config.manifest.bucket,
-        config.manifest.key.replace("/input/", "/enriched_manifest/"),
-        manifest4
+        config.manifest_bucket,
+        config.manifest_key.replace("/input/", "/enriched_manifest/"),
+        manifest4,
     )
 
     if upload_enriched_manifest:
@@ -285,14 +289,14 @@ def main(config: Config):
         logger.error("Manifest upload failed")
 
     logger.info("Tagging objects in S3")
-    tagged_objects = tag_objects(client, manifest4, config.nci_data_bucket.bucket)
+    tagged_objects = tag_objects(client, manifest4, config.nci_bucket)
 
     logger.info("Upload Tagging Report to S3")
     upload_tagging_report = upload_object(
         client,
-        config.manifest.bucket,
-        config.manifest.key.replace("/input/", "/tagging_report/"),
-        tagged_objects
+        config.manifest_bucket,
+        config.manifest_key.replace("/input/", "/tagging_report/"),
+        tagged_objects,
     )
 
     if upload_tagging_report:
@@ -302,5 +306,3 @@ def main(config: Config):
 
     logger.info("Kids First Object Tagger flow completed")
     return tagged_objects
-
-
