1157808411df9e5ebd82e44b2700429dd083f03e
bullenc <75388530+bullenc@users.noreply.github.com>
2025-05-27 12:53:37 -0500

    revert test

--- src/cog_igm_utils.py
+++ src/cog_igm_utils.py
@@ -347,8 +347,8 @@ def cog_igm_json2tsv(
         if parsing in ["cog_only", "cog_and_igm"]:
             if len(df_reshape) > 0:
                 cog_form_parser(df_reshape, dt, cog_op, logger)
-                #cog_transform_log = cog_transformer(df_reshape_file_name, cog_op) #testing
-                #print(f"COG transform log: {cog_transform_log}") #testing
+                cog_transform_log = cog_transformer(df_reshape_file_name, cog_op) 
+                print(f"COG transform log: {cog_transform_log}")
                 
             else:
                 logger.error(
@@ -445,7 +445,7 @@ def cog_igm_json2tsv(
         igm_success_count,
         igm_error_count,
         log_filename,
-        #cog_transform_log #testing
+        cog_transform_log 
     )
 
 
