21faef7abb362ce3fb43df6a25ad284612ac3370
Cole DeVries <colemandevries@gmail.com>
2025-07-24 11:11:32 -0400

    modifying the compare_dataframes workflow

--- workflows/compare_dataframes.py
+++ workflows/compare_dataframes.py
@@ -1,26 +1,41 @@
+import os
+import io
+import datetime
+from pytz import timezone
+import boto3
 import pandas as pd
 from prefect import task, flow, get_run_logger
-from datetime import datetime
-import os
-from src.utils import get_time, file_dl, folder_ul
+from botocore.exceptions import ClientError
 
-date_stamp = get_time()
+client = boto3.client("s3")
 
-output_dir = f"file_compare_{date_stamp}"
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+DATESTAMP = datetime.datetime.now(timezone("EST")).strftime("%Y%m%d_%H%M%S")
+OUTPUT_DIR = f"file_compare_{DATESTAMP}"
+
+
+def get_separator(file_path: str) -> str:
+    """Determine the separator based on the file extension."""
+    if file_path.lower().endswith(".csv"):
+        return ","
+    elif file_path.lower().endswith(".tsv"):
+        return "\t"
+    else:
+        raise ValueError("File must end with .csv or .tsv")
 
 
 @task
-def read_dataframe(file_path: str) -> pd.DataFrame:
-    """Read a TSV or CSV file into a pandas DataFrame."""
-    if file_path.endswith(".tsv"):
-        return pd.read_csv(file_path, sep="\t")
-    elif file_path.endswith(".csv"):
-        return pd.read_csv(file_path)
-    else:
-        raise ValueError("File must be a TSV or CSV format.")
+def get_object(bucket: str, key: str) -> pd.DataFrame:
+    """Loads and returns a DataFrame from a specified S3 Object (bucket, key)"""
+    separator = get_separator(key)
+
+    try:
+        response = client.get_object(Bucket=bucket, Key=key)
+        body = response["Body"].read().decode("utf-8")
+        df = pd.read_csv(io.StringIO(body), sep=separator)
+        return df
+    except ClientError as e:
+        raise Exception(f"Failed to download DataFrame from S3: {e}")
 
 
 @task
@@ -37,44 +52,31 @@ def compare_dataframes(
         how="outer",
         indicator=True,
     )
-
-    # Rows present in both DataFrames
     both_present = merged_df[merged_df["_merge"] == "both"]
-
-    # Rows only in df1
     only_in_df1 = merged_df[merged_df["_merge"] == "left_only"]
-
-    # Rows only in df2
     only_in_df2 = merged_df[merged_df["_merge"] == "right_only"]
 
     return both_present, only_in_df1, only_in_df2
 
 
 @task
-def save_dataframes(
-    both_present: pd.DataFrame, only_in_df1: pd.DataFrame, only_in_df2: pd.DataFrame
-) -> None:
-    """Save the comparison results to TSV files."""
-
-    both_present_file = f"both_present_{date_stamp}.tsv"
-    only_in_df1_file = f"only_in_df1_{date_stamp}.tsv"
-    only_in_df2_file = f"only_in_df2_{date_stamp}.tsv"
-
-    both_present.to_csv(
-        os.path.join(output_dir, both_present_file), sep="\t", index=False
-    )
-    only_in_df1.to_csv(
-        os.path.join(output_dir, only_in_df1_file), sep="\t", index=False
-    )
-    only_in_df2.to_csv(
-        os.path.join(output_dir, only_in_df2_file), sep="\t", index=False
-    )
+def upload_dataframe(df: pd.DataFrame, bucket: str, key: str) -> str:
+    """Uploads a provided DataFrame to an S3 bucket as a TSV file."""
+    buffer = df.to_csv(sep="\t", index=False)
+    try:
+        response = client.put_object(
+            Bucket=bucket, Key=key, Body=buffer.encode("utf-8")
+        )
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            return f"s3://{bucket}/{key}"
+    except ClientError as e:
+        raise Exception(f"Failed to upload DataFrame to S3: {e}")
 
 
 @flow(
     name="S3 DataFrame Comparison Flow",
     log_prints=True,
-    flow_run_name="{runner}_" + f"{get_time()}",
+    flow_run_name=f"{runner}_{DATESTAMP}",
 )
 def compare_dataframes_runner(
     bucket: str,
@@ -84,44 +86,37 @@ def compare_dataframes_runner(
     join_column_1: str,
     join_column_2: str,
 ):
-    """Flow to compare two dataframes from files in a specified S3 bucket.
+    """Runner function to compare two dataframes from files in a specified S3 bucket."""
 
-    Args:
-        bucket (str): Bucket name of where the manifest is located in and the output goes to
-        file_path_1 (str): File path of the first file to compare
-        file_path_2 (str): File path of the second file to compare
-        runner (str): Unique runner name (also becomes directory output path)
-        join_column_1 (str): Column name in the first DataFrame to join on
-        join_column_2 (str): Column name in the second DataFrame to join on
-    """
+    logger = get_run_logger()
+    logger.info(f"Retrieving file {file_path_1} from the {bucket} bucket.")
+    df1 = get_object(bucket, file_path_1)
 
-    # create a logging object
-    runner_logger = get_run_logger()
+    logger.info(f"Retrieving file {file_path_2} from the {bucket} bucket.")
+    df2 = get_object(bucket, file_path_2)
 
-    runner_logger.info(f"Obtaining files from bucket: {bucket}")
-    runner_logger.info(f"File 1 path: {file_path_1}")
-    # download file_1
-    file_dl(bucket, file_path_1)
-
-    runner_logger.info(f"File 2 path: {file_path_2}")
-    # download file_2
-    file_dl(bucket, file_path_2)
-
-    file_path_1 = os.path.basename(file_path_1)
-    file_path_2 = os.path.basename(file_path_2)
-
-    runner_logger.info(f"Reading dataframes from files: {file_path_1}, {file_path_2}")
-    df1 = read_dataframe(file_path_1)
-    df2 = read_dataframe(file_path_2)
-
-    runner_logger.info("Comparing dataframes...")
+    logger.info("Comparing dataframes...")
     both_present, only_in_df1, only_in_df2 = compare_dataframes(
         df1, df2, join_column_1, join_column_2
     )
 
-    runner_logger.info("Saving comparison results to TSV files...")
-    save_dataframes(both_present, only_in_df1, only_in_df2)
+    logger.info("Uploading comparison results to S3...")
+    base_path = f"{runner}/{OUTPUT_DIR}"
+    both_present_path = f"{base_path}/both_present_{DATESTAMP}.tsv"
+    only_in_df1_path = f"{base_path}/only_in_df1_{DATESTAMP}.tsv"
+    only_in_df2_path = f"{base_path}/only_in_df2_{DATESTAMP}.tsv"
 
-    runner_logger.info(f"Files uploaded to bucket: {bucket}/{runner}/{output_dir}")
-    # Upload the output files to S3
-    folder_ul(local_folder=output_dir, bucket=bucket, destination=runner, sub_folder="")
+    upload_both_present = upload_dataframe(both_present, bucket, both_present_path)
+    logger.info(f"Both present DataFrame uploaded to: {upload_both_present}")
+
+    upload_only_in_df1 = upload_dataframe(only_in_df1, bucket, only_in_df1_path)
+    logger.info(f"Only in DF1 DataFrame uploaded to: {upload_only_in_df1}")
+
+    upload_only_in_df2 = upload_dataframe(only_in_df2, bucket, only_in_df2_path)
+    logger.info(f"Only in DF2 DataFrame uploaded to: {upload_only_in_df2}")
+
+    return {
+        "both_present": upload_both_present,
+        "only_in_df1": upload_only_in_df1,
+        "only_in_df2": upload_only_in_df2,
+    }
