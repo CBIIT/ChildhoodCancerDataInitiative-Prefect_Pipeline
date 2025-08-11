41849a0cd1e4d72c4cd94a6ef5477bbf0c786532
svburke <42675174+svburke@users.noreply.github.com>
2025-08-11 09:04:00 -0500

    Update ccdi_cpi_query.py

--- workflows/ccdi_cpi_query.py
+++ workflows/ccdi_cpi_query.py
@@ -1,8 +1,6 @@
 from prefect import flow, task, get_run_logger
 import os
 import sys
-from src.neo4j_data_tools import pull_uniq_studies, export_to_csv_per_node_per_study, pull_data_per_node_per_study, cypher_query_parameters
-#from neo4j import GraphDatabase
 from src.utils import get_time, file_ul, get_secret, list_to_chunks, folder_ul, file_dl
 import pandas as pd
 import requests
@@ -11,16 +9,11 @@ import json
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir)
 
-
-# cypher_query_particiapnt_per_study = """
-# MATCH (startNode:{node_label})-[:of_{node_label}]-(linkedNode)-[*0..5]-(study:study {{study_id:"{study_accession}"}})
-# RETURN startNode.{node_label}_id as {node_label}_id
-# """
-
 API_DOMAIN = "https://participantindex.ccdi.cancer.gov"
 API_GET_DOMAINS = "/v1/domains"
 API_GET_RELEVANT_DOMAINS = "/v1/participant_ids/domains"
 API_GET_ASSOCIATED_PARTICIPANT_IDS = "/v1/associated_participant_ids"
+
 
 @task(name="Get access token for CPI API", log_prints=True)
 def get_access_token(client_id: str, client_secret: str, token_url: str) -> str:
@@ -44,12 +37,13 @@ def get_access_token(client_id: str, client_secret: str, token_url: str) -> str:
 
     if response.status_code == 200:
         access_token = response.json().get("access_token")
-        #print(f"Debug: Access Token - {access_token}")
+        # print(f"Debug: Access Token - {access_token}")
         return access_token
     else:
         raise Exception(
             f"Failed to get access token: {response.status_code} - {response.text}"
         )
+
 
 @task(name="Get request return for CPI", log_prints=True, cache_key_fn=None)
 def get_cpi_request(api_extension: str, access_token: str, request_body: str) -> dict:
@@ -71,7 +65,7 @@ def get_cpi_request(api_extension: str, access_token: str, request_body: str) ->
         "Content-Type": "application/json",
         "Accept": "application/json",  # Matching Postman behavior
     }
-    
+
     headers = {
         "Authorization": f"Bearer {access_token}",  # Ensure correct prefix
         "Content-Type": "application/json",
@@ -86,6 +80,7 @@ def get_cpi_request(api_extension: str, access_token: str, request_body: str) ->
     else:
         raise Exception(f"API request failed: {response.status_code} - {response.text}")
 
+
 @flow(name="Get all domain information", log_prints=True)
 def get_all_domain_info(client_id: str, client_secret: str, token_url: str) -> str:
     """Get all domain information from CPI API
@@ -97,7 +92,7 @@ def get_all_domain_info(client_id: str, client_secret: str, token_url: str) -> s
 
     Returns:
         str: domain information file name
-    """    
+    """
     access_token = get_access_token(
         client_id=client_id, client_secret=client_secret, token_url=token_url
     )
@@ -129,7 +124,14 @@ def reformat_domain_dict(filepath: str) -> dict:
 
 
 @flow(name="Fetch associated ids for participants", log_prints=True)
-def get_associated_ids(filepath: str, out_dir: str, domain_file: str, client_id: str, client_secret: str, token_url: str) -> str:
+def get_associated_ids(
+    filepath: str,
+    out_dir: str,
+    domain_file: str,
+    client_id: str,
+    client_secret: str,
+    token_url: str,
+) -> str:
     """Read a tsv and get associatd id from different domains and returns a file per study
 
     Args:
@@ -139,7 +141,7 @@ def get_associated_ids(filepath: str, out_dir: str, domain_file: str, client_id:
         client_id (str): cpi api client id
         client_secret (str): cpi api client secret
         token_url (str): cpi api token url
-    
+
     Returns:
         str: filepath of the output file
     """
@@ -156,11 +158,9 @@ def get_associated_ids(filepath: str, out_dir: str, domain_file: str, client_id:
     study_id = study_id["study_id"].unique().tolist()[0]
     participant_list = participant_list[participant_list["participant_id"].notna()]
 
-
     id_df = participant_list
-    id_df['study_id'] = study_id
+    id_df["study_id"] = study_id
 
-    #id_df = pd.read_csv(filepath, sep="\t")
     uniq_studies = list(id_df["study_id"].unique())
 
     for study in uniq_studies:
@@ -242,10 +242,10 @@ def get_associated_ids(filepath: str, out_dir: str, domain_file: str, client_id:
                             )
                     else:
                         pass
-                    
+
                 else:
                     pass
-                
+
             # append the dataframe to exisitng file
             if len(records_to_write) > 0:
                 study_df = pd.read_csv(study_filepath, sep="\t")
@@ -260,7 +260,14 @@ def get_associated_ids(filepath: str, out_dir: str, domain_file: str, client_id:
 
 
 @flow(name="Get Associated Domains of CCDI Participants", log_prints=True)
-def get_ccdi_cpi_ids(bucket: str, runner: str, uri_parameter: str, file_path: str, username_parameter: str, password_parameter: str) -> None:
+def get_ccdi_cpi_ids(
+    bucket: str,
+    runner: str,
+    file_path: str,
+    uri_parameter: str,
+    username_parameter: str,
+    password_parameter: str,
+) -> None:
     """Get Associated id(s) of all CCDI participants in sandbox DB using CPI API
 
     Args:
@@ -270,7 +277,7 @@ def get_ccdi_cpi_ids(bucket: str, runner: str, uri_parameter: str, file_path: st
         uri_parameter (str): db uri parameter
         username_parameter (str): db username parameter
         password_parameter (str): db password parameter
-    """    
+    """
     logger = get_run_logger()
     logger.info("Getting uri, username and password parameter from AWS")
 
@@ -279,20 +286,35 @@ def get_ccdi_cpi_ids(bucket: str, runner: str, uri_parameter: str, file_path: st
     file_dl(bucket=bucket, filename=file_path)
     filename = os.path.basename(file_path)
     logger.info(f"Downloaded file {filename}")
-    
+
     # get secrets from AWS secret manager
-    client_id = get_secret(secret_name_path="ccdi/nonprod/inventory/cpi_api_creds", secret_key_name="client_id")
-    #print(client_id)
-    secret = get_secret(secret_name_path="ccdi/nonprod/inventory/cpi_api_creds", secret_key_name="secret")
-    #print(secret)
-    access_token_url = get_secret(secret_name_path="ccdi/nonprod/inventory/cpi_api_creds", secret_key_name="access_token_url")
-    #print(access_token_url)
+    client_id = get_secret(
+        secret_name_path="ccdi/nonprod/inventory/cpi_api_creds",
+        secret_key_name="client_id",
+    )
+    # print(client_id)
+    secret = get_secret(
+        secret_name_path="ccdi/nonprod/inventory/cpi_api_creds",
+        secret_key_name="secret",
+    )
+    # print(secret)
+    access_token_url = get_secret(
+        secret_name_path="ccdi/nonprod/inventory/cpi_api_creds",
+        secret_key_name="access_token_url",
+    )
+    # print(access_token_url)
 
     # get all domain information
-    domain_file = get_all_domain_info(client_id=client_id, client_secret=secret, token_url=access_token_url)
+    domain_file = get_all_domain_info(
+        client_id=client_id, client_secret=secret, token_url=access_token_url
+    )
     logger.info("Fetched all domain metadata from CPI API")
-    file_ul(bucket=bucket, output_folder=upload_folder, sub_folder="", newfile=domain_file)
-    logger.info(f"Uploaded all domain metadata to the bucket {bucket} at {upload_folder})")
+    file_ul(
+        bucket=bucket, output_folder=upload_folder, sub_folder="", newfile=domain_file
+    )
+    logger.info(
+        f"Uploaded all domain metadata to the bucket {bucket} at {upload_folder})"
+    )
 
     # get associated ids for all the participants
     out_dir = "associated_participant_ids_" + get_time()
@@ -308,8 +330,11 @@ def get_ccdi_cpi_ids(bucket: str, runner: str, uri_parameter: str, file_path: st
         client_secret=secret,
         token_url=access_token_url,
     )
-    #logger.info(f"Fetched asscociated ids for all participants in the tsv {participant_filename}")
-    folder_ul(local_folder=out_dir, bucket=bucket, destination=upload_folder, sub_folder="")
-    logger.info(f"Uploaded associated ids for all participants to the bucket {bucket} folder path {upload_folder}")
+    folder_ul(
+        local_folder=out_dir, bucket=bucket, destination=upload_folder, sub_folder=""
+    )
+    logger.info(
+        f"Uploaded associated ids for all participants to the bucket {bucket} folder path {upload_folder}"
+    )
 
     return None
