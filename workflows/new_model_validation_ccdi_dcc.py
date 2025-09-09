fed208c53cc2b7a2be4338ded03e9691c39856e0
svburke <42675174+svburke@users.noreply.github.com>
2025-09-09 15:43:50 -0500

    Update new_model_validation_ccdi_dcc.py

--- workflows/new_model_validation_ccdi_dcc.py
+++ workflows/new_model_validation_ccdi_dcc.py
@@ -4,7 +4,7 @@ import traceback
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir)
-from model_to_submission import create_submission_manifest
+from model_to_submission_ccdi_dcc import create_submission_manifest
 from src.template_exampler_ccdi_dcc import make_template_example
 from src.s3_validationry_refactored import ValidationRy_new
 from src.utils import get_date, get_time, file_ul
