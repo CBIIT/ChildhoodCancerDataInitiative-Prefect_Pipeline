b8da7fc4dfe4e26df42221b2e817ce074340ee19
bullenc <75388530+bullenc@users.noreply.github.com>
2025-10-22 14:04:11 -0500

    Update pull_n_join_manifest_single_study.py

--- workflows/pull_n_join_manifest_single_study.py
+++ workflows/pull_n_join_manifest_single_study.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 from workflows.pull_neo4j_data import pull_neo4j_data
+from workflows.db_tsv_to_manifest import join_tsv_to_manifest
 
 
 @flow(
@@ -14,7 +15,8 @@ from workflows.pull_neo4j_data import pull_neo4j_data
 def pull_n_join_manifest_single_study(
     bucket: str,
     runner: str,
-    study_id: str
+    study_id: str,
+    ccdi_template_tag: str
 ):
     """Pipeline that pulls ingested study from a Neo4j database for study phs ID provided.
 
@@ -22,6 +24,7 @@ def pull_n_join_manifest_single_study(
         bucket (str): The S3 bucket to pull data from.
         runner (str): The runner to use for the workflow.
         study_id (str): The study ID to pull data for.
+        ccdi_template_tag (str): Tag name of CCDI manifest
     """
     
     logger = get_run_logger()
@@ -33,7 +36,16 @@ def pull_n_join_manifest_single_study(
         study_id=study_id,
     )
     
-    logger.info(f"Pulled data stored at {op_folder}")
+    op_folder_path = f"{op_folder}/{study_id}"
+
+    logger.info(f"Pulled data stored at {op_folder_path}")
+    
+    join_tsv_to_manifest(
+        bucket=bucket,
+        runner=runner,
+        tsv_folder_path=op_folder_path,
+        ccdi_template_tag=ccdi_template_tag
+    )
 
 if __name__ == "__main__":
 
