6dc9663bee2f260be1004348049ebdeabbb20782
svburke <42675174+svburke@users.noreply.github.com>
2025-11-12 10:05:04 -0600

    Update hello_world.py

--- workflows/hello_world.py
+++ workflows/hello_world.py
@@ -5,13 +5,14 @@ def greeting(name):
     print(f"Hello {name}!")
 
 @flow(log_prints = True)
-"""
+
+def hello_flow(name: str = "World"):
+    """
 A simple script that prints "Hello" followed by a user-specified name.
 Args:
     name (str): the user-specified name as a string, ie "Jane Doe". 
     Defaults to "World".
-""" 
-def hello_flow(name: str = "World"):
+    """ 
     greeting(name)
 
 if __name__ == "__main__":
