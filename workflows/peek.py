84cb214c4968cd05d70396e866ee2e77dbd65cb2
Mark A. Jensen <mark.jensen@nih.gov>
2025-11-28 15:57:14 -0500

    rm dep

--- workflows/peek.py
+++ workflows/peek.py
@@ -4,7 +4,6 @@ import sys
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir)
-from src.file_mover import move_manifest_files
 from src.utils import get_time, file_dl, file_ul
 
 
