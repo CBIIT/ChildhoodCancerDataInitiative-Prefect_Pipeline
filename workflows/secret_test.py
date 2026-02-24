c8d7519103a2f0a16cfa1b93ac990bf0d52ce2c5
svburke <42675174+svburke@users.noreply.github.com>
2026-02-24 11:57:12 -0600

    move func to utills

--- workflows/secret_test.py
+++ workflows/secret_test.py
@@ -2,32 +2,7 @@ from prefect import flow, task
 import boto3
 from botocore.exceptions import ClientError
 import json
-
-@task(name="ccdi get secret from secrets manager", log_prints=True)
-def get_secret(secret_name_path: str, secret_key_name: str, account: str):
-    """Retrieve a secret hash from AWS Secrets Manager
-
-    Args:
-        secret_name_path (str): Secrets name path, i.e. ccdi/storage/inventory/token
-        secret_key_name (str): Secret key name associated with hash/token
-        account (str): AWS account identifier
-    Returns:
-        str: Secret hash/token
-    """
-    region_name = "us-east-1"
-    secret_name_path = f"arn:aws:secretsmanager:{region_name}:{account}:secret:{secret_name_path}"
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(service_name="secretsmanager", region_name=region_name)
-    try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name_path)
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    return json.loads(get_secret_value_response["SecretString"])[secret_key_name]
-
+from utils import get_secret_centralized_worker
 
 @flow(name="TEST Secret Retrieval Pipeline", log_prints=True)
 def secret_pipeline(secret_name_path: str, secret_key_name: str, account: str) -> None:
@@ -39,7 +14,7 @@ def secret_pipeline(secret_name_path: str, secret_key_name: str, account: str) -
         secret_key_name: Name of the key within the secret to retrieve
         account: AWS account identifier
     """
-    secret_value = get_secret(secret_name_path, secret_key_name, account)
+    secret_value = get_secret_centralized_worker(secret_name_path, secret_key_name, account)
     # test if secret value is a string and has length greater than 0
     if isinstance(secret_value, str) and len(secret_value) > 0:
         print("$$$$$$$$$ We successfully retrieved the secret value! $$$$$$$$$")
