9806fcdd6891331a1b378f87bfc69f74d5173cb4
qiongL910 <liuqiong891023@gmail.com>
2026-03-17 12:34:15 -0400

    changed the rel multiplicity condition to parse linkage value

--- src/s3_validationry_refactored.py
+++ src/s3_validationry_refactored.py
@@ -1846,12 +1846,12 @@ def validate_cross_links_single_sheet(node_name: str, file_object, model_rel_lis
         # find the unique values of that linking property
         link_values = node_df[link_prop].dropna().unique().tolist()
 
-        # mul_type_to_parse = ["many_to_many", "one_to_many"]
-        mul_type_to_parse = [
-            "many_to_many",
-            "one_to_many",
-            "many_to_one",
-        ]  # only used for testing the block below
+        mul_type_to_parse = ["many_to_many", "one_to_many"]
+        #mul_type_to_parse = [
+        #    "many_to_many",
+        #    "one_to_many",
+        #    "many_to_one",
+        #]  # only used for testing the block below
         if link_mul in mul_type_to_parse:
             # only parse link values if the multiplicity is many_to_many or one_to_many
             parsed_unique_link_values = []
