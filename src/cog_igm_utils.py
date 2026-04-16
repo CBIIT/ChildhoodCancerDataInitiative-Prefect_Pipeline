fe1592bae0fba40ea26c913f78b633ee1ca48f2f
bullenc <75388530+bullenc@users.noreply.github.com>
2026-04-16 12:35:16 -0500

    Update cog_igm_utils.py

--- src/cog_igm_utils.py
+++ src/cog_igm_utils.py
@@ -406,6 +406,10 @@ def percent_necrosis_tumor_fill_in(manifest_path: str, decoded_tsv_path: list[st
     # read in sample sheet
     sample_df = pd.read_excel(manifest_path, sheet_name="sample", engine="openpyxl")
     
+    # set sample_id as string to avoid issues with leading zeros
+    sample_df['sample_id'] = sample_df['sample_id'].astype(str)
+    merged_df['sample.sample_id'] = merged_df['sample.sample_id'].astype(str)
+    
     # grab col order of sample_df to reset after merge
     col_order = sample_df.columns.tolist()
     
