faf8b8a9b9df4d22ad2f269a4b13cf8ccf40a50d
svburke <42675174+svburke@users.noreply.github.com>
2026-04-17 08:31:24 -0500

    Update pull_db_data.py

--- workflows/pull_db_data.py
+++ workflows/pull_db_data.py
@@ -70,6 +70,15 @@ def pull_db_data(
         study_id_list=study_id_list
     )
 
+    # upload converted tsv files to the bucket
+    logger.info(f"Uploading folder of {db_data_folder} to the bucket {bucket} at {bucket_folder}")
+    folder_ul(
+        local_folder=db_data_folder,
+        bucket=bucket,
+        destination=bucket_folder,
+        sub_folder="",
+    )
+
     # converting data pulled from DB (csv files) to tsv files
     logger.info("Starting to convert DB pulled csv to tsv files")
     export_folder = convert_csv_to_tsv_dcc(db_pulled_outdir=db_data_folder, output_dir="./")
