66d84ccd59bb4437236a981a0dca70ac701a633a
svburke <42675174+svburke@users.noreply.github.com>
2026-07-17 13:55:34 -0500

    Update create_submission_ccdi_dcc.py

--- src/create_submission_ccdi_dcc.py
+++ src/create_submission_ccdi_dcc.py
@@ -540,7 +540,7 @@ class ManifestSheet:
         # format required and nonrequired property cell
         row_number = 1
         for row in dataframe_to_rows(dict_df, index=False, header=True):
-            if row[0] in ["file_url", "dcf_indexd_guid", "authz", "acl"]:
+            if row[0] in ["file_url", "dcf_indexd_guid", "authz", "acl", "study_status_id","repository_name","version","release_status","number_of_participants","number_of_samples","number_of_files","total_size_of_data_files","promotion_status"]:
                 sheet_dictionary["A" + str(row_number)].font = (
                     ManifestStyle.meta_index_font
                 )
@@ -828,7 +828,7 @@ class ManifestSheet:
                 logger=logger,
             )
             if if_h_prop_req:
-                if h_prop in ["guid","file_url", "dcf_indexd_guid", "authz", "acl"]:
+                if h_prop in ["guid","file_url", "dcf_indexd_guid", "authz", "acl", "study_status_id","repository_name","version","release_status","number_of_participants","number_of_samples","number_of_files","total_size_of_data_files","promotion_status"]:
                     ws_node[cell_h_col].fill = ManifestStyle.meta_index_pattern
                     ws_node[cell_h_col].font = ManifestStyle.meta_index_font
                 else:
