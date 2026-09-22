9f49a9ae955b122aaaa025d930df2318a9c5f76a
svburke <42675174+svburke@users.noreply.github.com>
2026-09-22 14:34:36 -0500

    init

--- src/s3_catcherry.py
+++ src/s3_catcherry.py
@@ -305,6 +305,57 @@ def CatchERRy(file_path: str, template_path: str):  # removed profile
 
         ##############
         #
+        # MCI submitted_diagnosis to Harmonized diagnosis transformation
+        #
+        ##############
+
+        # Clean up diagnosis values based on the MCI_invalidDiagnosisMappings.tsv file found in the docs folder.
+        mci_invalid_diagnosis_mapping_path = (
+            "docs/MCI_InvalidDiagnosisMappings.tsv"
+        )
+        mci_invalid_diagnosis_mapping = pd.read_csv(
+            mci_invalid_diagnosis_mapping_path, sep="\t", dtype=str
+        )
+
+        # create mapping dictionary for submitted_diagnosis to diagnosis
+        mci_invalid_diagnosis_dict = mci_invalid_diagnosis_mapping.set_index(
+            "submitted_diagnosis"
+        )["diagnosis"].to_dict()
+
+        catcherr_logger.info(
+            "Cleaning up diagnosis values based on MCI_InvalidDiagnosisMappings.tsv"
+        )
+
+        for node in dict_nodes:
+            df = meta_dfs[node]
+            if "submitted_diagnosis" in df.columns:
+                for index, row in df.iterrows():
+                    submitted_diagnosis_value = row["submitted_diagnosis"]
+                    if pd.notna(submitted_diagnosis_value):
+                        if ";" in submitted_diagnosis_value:
+                            submitted_diagnosis_list = submitted_diagnosis_value.split(";")
+                            new_diagnosis_list = []
+                            for submitted_diagnosis in submitted_diagnosis_list:
+                                new_diagnosis = mci_invalid_diagnosis_dict.get(
+                                    submitted_diagnosis, submitted_diagnosis
+                                )
+                                new_diagnosis_list.append(new_diagnosis)
+                                # only apply laterality if the submitted value had a mapping
+                                if submitted_diagnosis in mci_invalid_laterality_dict:
+                                    new_laterality = mci_invalid_laterality_dict.get(submitted_diagnosis)
+                                    if pd.notna(new_laterality):
+                                        new_laterality_list.append(new_laterality)
+                            df.at[index, "diagnosis"] = ";".join(new_diagnosis_list)
+                        else:
+                            new_diagnosis = mci_invalid_diagnosis_dict.get(
+                                submitted_diagnosis_value, submitted_diagnosis_value
+                            )
+                            df.at[index, "diagnosis"] = new_diagnosis
+            meta_dfs[node] = df
+
+
+        ##############
+        #
         # MCI Anatomic_site to Harmonized Anatomic_site transformation
         #
         ##############
